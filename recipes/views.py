76bd833eba6f001c63d31c50959ac04f4df22fca
Lucas Martins Teixeira <lucaslag.contact@gmail.com>
2023-11-08 15:41:22 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Lucas Martins'
+    })
