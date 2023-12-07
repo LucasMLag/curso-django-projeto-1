ead01dc157048d1624b88ef9e146571a2d1c657f
Lucas Martins Teixeira <lucaslag.contact@gmail.com>
2023-12-07 07:20:12 -0300

    Projeto renomeado para project

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 
-from .models import Recipe
+from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -16,7 +18,8 @@ def category(request, category_id):
         Recipe.objects.filter(
             category__id=category_id,
             is_published=True,
-        ).order_by('-id'))
+        ).order_by('-id')
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
