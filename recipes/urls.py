1d45074da75678adc442d21bb7df958cf9b1e20a
Lucas Martins Teixeira <lucaslag.contact@gmail.com>
2024-01-07 23:29:43 -0300

    Refatorando com Genereric API Views e ViewSet

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
 
 from . import views
+from rest_framework.routers import SimpleRouter
 
-# recipes:recipe
 app_name = 'recipes'
+
+recipe_api_v2_router = SimpleRouter()
+recipe_api_v2_router.register(
+    'recipes/api/v2',
+    views.RecipeAPIv2ViewSet,
+    basename='recipes-api'
+    # with the basename, these are the view names:
+    # recipes:recipes-api-list
+    # recipes:recipes-api-detail pk
+)
+
+
+# This could be used if we were only using the router urls
+# urlpatterns = recipe_api_v2_router.urls
 
 urlpatterns = [
     path('', views.RecipeListViewHome.as_view(), name="home"),
@@ -13,7 +27,20 @@ urlpatterns = [
     path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
     path('recipes/api/v1/', views.RecipeListViewHomeApi.as_view(), name="recipes_api_v1"),
     path('recipes/api/v1/<int:pk>/', views.RecipeDetailApi.as_view(), name="recipes_api_v1_detail"),
-    path('recipes/api/v2/', views.RecipeAPIv2List.as_view(), name="recipes_api_v2"),
-    path('recipes/api/v2/<int:pk>/', views.RecipeAPIv2Detail.as_view(), name="recipes_api_v2_detail"),
+
+    # <!> This code was replaced by the recipe_api_v2_router
+    # path('recipes/api/v2/', views.RecipeAPIv2ViewSet.as_view({
+    #     'get': 'list',
+    #     'post': 'create',
+    # }), name="recipes_api_v2"),
+    # path('recipes/api/v2/<int:pk>/', views.RecipeAPIv2ViewSet.as_view({
+    #     'get': 'retrieve',
+    #     'patch': 'partial_update',
+    #     'delete': 'destroy',
+    # }), name="recipes_api_v2_detail"),
+    # <!>
+
     path('recipes/api/v2/tag/<int:pk>/', views.tag_api_detail, name="recipes_api_v2_tag"),
 ]
+
+urlpatterns += recipe_api_v2_router.urls
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,68 +1,36 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from rest_framework import status
 from tag.models import Tag
-from rest_framework.views import APIView
+# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
 from ..serializers import TagSerializer
 from ..models import Recipe
 from ..serializers import RecipeSerializer
 
 
-class RecipeAPIv2List(APIView):
-    def get(self, request):
-        recipes = Recipe.objects.get_published()[:10]
-        serializer = RecipeSerializer(
-            instance=recipes,
-            many=True,
-            context={'request': request},
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data,
-            context={'request': request},
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 9
 
 
-class RecipeAPIv2Detail(APIView):
-    def get_recipe(self, pk):
-        recipe = get_object_or_404(
-            Recipe.objects.all(),
-            pk=pk
-        )
-        return recipe
+# To use this class it is required to configure the routing on urls.py
+class RecipeAPIv2ViewSet(ModelViewSet):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
 
-    def get(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            context={'request': request},
-        )
-        return Response(serializer.data)
 
-    # Put/Patch needs both to read and write data, so the serializer needs instance=object and data=request.data
-    def patch(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(
-            instance=recipe,
-            data=request.data,
-            context={'request': request},
-            partial=True,  # Tells the serializer this is a Patch
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# Those two classes could be used instead to remove the need of routing in the urls.py
+# class RecipeAPIv2List(ListCreateAPIView):
+#     queryset = Recipe.objects.get_published()
+#     serializer_class = RecipeSerializer
+#     pagination_class = RecipeAPIv2Pagination
+# class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+#     queryset = Recipe.objects.get_published()
+#     serializer_class = RecipeSerializer
+#     pagination_class = RecipeAPIv2Pagination
 
 
 @api_view()
